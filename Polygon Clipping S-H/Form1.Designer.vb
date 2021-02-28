31b61c29ffaf2c3522c9a83ed4115a0a502fac81
Francis-Dwiputra-A <58261826+Francis-Dwiputra-A@users.noreply.github.com>
2021-02-28 21:42:55 +0700

    Week 1 ish

---
+++ Polygon Clipping S-H/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pbCanvas = New System.Windows.Forms.PictureBox()
+        Me.btnPoly = New System.Windows.Forms.Button()
+        Me.btnRect = New System.Windows.Forms.Button()
+        Me.txtbox = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.pbCanvas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbCanvas
+        '
+        Me.pbCanvas.Location = New System.Drawing.Point(12, 12)
+        Me.pbCanvas.Name = "pbCanvas"
+        Me.pbCanvas.Size = New System.Drawing.Size(809, 469)
+        Me.pbCanvas.TabIndex = 0
+        Me.pbCanvas.TabStop = False
+        '
+        'btnPoly
+        '
+        Me.btnPoly.Location = New System.Drawing.Point(902, 70)
+        Me.btnPoly.Name = "btnPoly"
+        Me.btnPoly.Size = New System.Drawing.Size(188, 60)
+        Me.btnPoly.TabIndex = 1
+        Me.btnPoly.Text = "Polygon"
+        Me.btnPoly.UseVisualStyleBackColor = True
+        '
+        'btnRect
+        '
+        Me.btnRect.Location = New System.Drawing.Point(902, 219)
+        Me.btnRect.Name = "btnRect"
+        Me.btnRect.Size = New System.Drawing.Size(188, 60)
+        Me.btnRect.TabIndex = 1
+        Me.btnRect.Text = "Rectangle"
+        Me.btnRect.UseVisualStyleBackColor = True
+        '
+        'txtbox
+        '
+        Me.txtbox.Location = New System.Drawing.Point(888, 359)
+        Me.txtbox.Name = "txtbox"
+        Me.txtbox.Size = New System.Drawing.Size(234, 22)
+        Me.txtbox.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(888, 411)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(234, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1173, 571)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtbox)
+        Me.Controls.Add(Me.btnRect)
+        Me.Controls.Add(Me.btnPoly)
+        Me.Controls.Add(Me.pbCanvas)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbCanvas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbCanvas As PictureBox
+    Friend WithEvents btnPoly As Button
+    Friend WithEvents btnRect As Button
+    Friend WithEvents txtbox As TextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Polygon Clipping S-H/Form1.vb
@@ -0,0 +1,266 @@
+﻿Public Class Form1
+    Dim SelectRect As Rectangle = New Rectangle()
+    Dim _pen As Pen = New Pen(Color.Black, 3)
+    Dim shape As String
+    ' Each polygon is represented by a List(Of Point).
+    Private Polygons As List(Of Point) = Nothing
+
+    ' Points for the new polygon.
+    Private NewPolygon As List(Of Point) = Nothing
+
+    ' The current mouse position while drawing a new polygon.
+    Private NewPoint As Point
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        pbCanvas.BackColor = Color.White
+
+    End Sub
+
+    Private Sub pbCanvas_MouseDown(sender As Object, e As MouseEventArgs) Handles pbCanvas.MouseDown
+        If (shape = "Polygon") Then
+            ' See if we are already drawing a polygon.
+            If (NewPolygon IsNot Nothing) Then
+                ' We are already drawing a polygon.
+                ' If it's the right mouse button, finish this
+                ' polygon.
+                If (e.Button = MouseButtons.Right) Then
+                    ' Finish this polygon.
+                    If (NewPolygon.Count > 2) Then
+                        Polygons = NewPolygon
+                        NewPolygon = Nothing
+                    End If
+                Else
+                    ' Add a point to this polygon.
+                    If (NewPolygon(NewPolygon.Count - 1) <>
+            e.Location) Then
+                        NewPolygon.Add(e.Location)
+                    End If
+                End If
+            Else
+                ' Start a new polygon.
+                NewPolygon = New List(Of Point)()
+                NewPoint = e.Location
+                NewPolygon.Add(e.Location)
+            End If
+        Else
+            SelectRect.Location = e.Location
+            SelectRect.Size = New Size(0, 0)
+        End If
+        ' Redraw.
+        pbCanvas.Invalidate()
+    End Sub
+
+    Private Sub pbCanvas_MouseMove(sender As Object, e As MouseEventArgs) Handles pbCanvas.MouseMove
+        If (shape = "Polygon") Then
+            If (NewPolygon Is Nothing) Then Exit Sub
+            NewPoint = e.Location
+
+        Else
+            If (e.Button = MouseButtons.Left) Then
+
+                SelectRect.Width = e.X - SelectRect.X
+                SelectRect.Height = e.Y - SelectRect.Y
+
+            End If
+        End If
+        pbCanvas.Invalidate()
+    End Sub
+
+    Private Function Clipping(Poly As Array) As List(Of Point)
+        Dim top = SelectRect.Top
+        Dim bottom = SelectRect.Bottom
+        Dim right = SelectRect.Right
+        Dim left = SelectRect.Left
+        Dim Clipped As New List(Of Point)
+        Dim NewClipped As New List(Of Point)
+        Dim counter = 0
+        For count As Integer = 0 To Poly.Length - 2
+            'Top Clipping'
+            If ((Poly(count).Y < top And Poly(count + 1).Y > top) Or (Poly(count).Y > top And Poly(count + 1).Y < top)) Then
+                Dim dx = Poly(count + 1).X - Poly(count).X
+                Dim dy = Poly(count + 1).Y - Poly(count).Y
+                Dim x = Poly(count).X + dx * (top - Poly(count).Y) / dy
+                Dim y = top
+                Dim p As New Point(x, y)
+                If (Poly(count).Y < top And Poly(count + 1).Y > top) Then
+                    Clipped.Add(p)
+                    Clipped.Add(Poly(count + 1))
+                Else
+                    Clipped.Add(p)
+                End If
+            ElseIf (Poly(count).Y > top And Poly(count + 1).Y > top) Then
+                Clipped.Add(Poly(count + 1))
+            End If
+        Next
+
+        If ((Poly(Poly.Length - 1).Y > top And Poly(0).Y < top) Or (Poly(Poly.Length - 1).Y < top And Poly(0).Y > top)) Then
+            Dim dx = Poly(0).X - Poly(Poly.Length - 1).X
+            Dim dy = Poly(0).Y - Poly(Poly.Length - 1).Y
+            Dim x = Poly(Poly.Length - 1).X + dx * (top - Poly(Poly.Length - 1).Y) / dy
+            Dim y = top
+            Dim p As New Point(x, y)
+            If (Poly(Poly.Length - 1).Y < top And Poly(0).Y > top) Then
+                Clipped.Add(p)
+                Clipped.Add(Poly(0))
+            Else
+                Clipped.Add(p)
+            End If
+        ElseIf (Poly(Poly.Length - 1).Y > top And Poly(0).Y > top) Then
+            Clipped.Add(Poly(0))
+        End If
+
+        For count As Integer = 0 To Clipped.ToArray.Length - 2
+            'Bottom Clipping'
+            If ((Clipped(count).Y < bottom And Clipped(count + 1).Y > bottom) Or (Clipped(count).Y > bottom And Clipped(count + 1).Y < bottom)) Then
+                Dim dx = Clipped(count + 1).X - Clipped(count).X
+                Dim dy = Clipped(count + 1).Y - Clipped(count).Y
+                Dim x = Clipped(count).X + dx * (bottom - Clipped(count).Y) / dy
+                Dim y = bottom
+                Dim p As New Point(x, y)
+                If (Clipped(count).Y > bottom And Clipped(count + 1).Y < bottom) Then
+                    NewClipped.Add(p)
+                    NewClipped.Add(Clipped(count + 1))
+                Else
+                    NewClipped.Add(p)
+                End If
+            ElseIf (Clipped(count).Y < bottom And Clipped(count + 1).Y < bottom) Then
+                NewClipped.Add(Clipped(count + 1))
+            End If
+        Next
+
+        If ((Clipped(Clipped.ToArray.Length - 1).Y > bottom And Clipped(0).Y < bottom) Or (Clipped(Clipped.ToArray.Length - 1).Y < bottom And Clipped(0).Y > bottom)) Then
+            Dim dx = Clipped(0).X - Clipped(Clipped.ToArray.Length - 1).X
+            Dim dy = Clipped(0).Y - Clipped(Clipped.ToArray.Length - 1).Y
+            Dim x = Clipped(Clipped.ToArray.Length - 1).X + dx * (bottom - Clipped(Clipped.ToArray.Length - 1).Y) / dy
+            Dim y = bottom
+            Dim p As New Point(x, y)
+            If (Clipped(Clipped.ToArray.Length - 1).Y > bottom And Clipped(0).Y < bottom) Then
+                NewClipped.Add(p)
+                NewClipped.Add(Clipped(0))
+            Else
+                NewClipped.Add(p)
+            End If
+        ElseIf (Clipped(Clipped.ToArray.Length - 1).Y < bottom And Clipped(0).Y < bottom) Then
+            NewClipped.Add(Clipped(0))
+        End If
+        Clipped = NewClipped
+        NewClipped = New List(Of Point)
+
+        For count As Integer = 0 To Clipped.ToArray.Length - 2
+            'right Clipping'
+            If ((Clipped(count).X < right And Clipped(count + 1).X > right) Or (Clipped(count).X > right And Clipped(count + 1).X < right)) Then
+                Dim dx = Clipped(count + 1).X - Clipped(count).X
+                Dim dy = Clipped(count + 1).Y - Clipped(count).Y
+                Dim x = right
+                Dim y = Clipped(count).Y + dy * (right - Clipped(count).X) / dx
+                Dim p As New Point(x, y)
+                If (Clipped(count).X > right And Clipped(count + 1).X < right) Then
+                    NewClipped.Add(p)
+                    NewClipped.Add(Clipped(count + 1))
+                Else
+                    NewClipped.Add(p)
+                End If
+            ElseIf (Clipped(count).X < right And Clipped(count + 1).X < right) Then
+                NewClipped.Add(Clipped(count + 1))
+            End If
+        Next
+
+        If ((Clipped(Clipped.ToArray.Length - 1).X > right And Clipped(0).X < right) Or (Clipped(Clipped.ToArray.Length - 1).X < right And Clipped(0).X > right)) Then
+            Dim dx = Clipped(0).X - Clipped(Clipped.ToArray.Length - 1).X
+            Dim dy = Clipped(0).Y - Clipped(Clipped.ToArray.Length - 1).Y
+            Dim y = Clipped(Clipped.ToArray.Length - 1).Y + dy * (right - Clipped(Clipped.ToArray.Length - 1).X) / dx
+            Dim x = right
+            Dim p As New Point(x, y)
+            If (Clipped(Clipped.ToArray.Length - 1).X > right And Clipped(0).X < right) Then
+                NewClipped.Add(p)
+                NewClipped.Add(Clipped(0))
+            Else
+                NewClipped.Add(p)
+            End If
+        ElseIf (Clipped(Clipped.ToArray.Length - 1).X < right And Clipped(0).X < right) Then
+            NewClipped.Add(Clipped(0))
+        End If
+
+        Clipped = NewClipped
+        NewClipped = New List(Of Point)
+
+        For count As Integer = 0 To Clipped.ToArray.Length - 2
+            'Left Clipping'
+            If ((Clipped(count).X < left And Clipped(count + 1).X > left) Or (Clipped(count).X > left And Clipped(count + 1).X < left)) Then
+                Dim dx = Clipped(count + 1).X - Clipped(count).X
+                Dim dy = Clipped(count + 1).Y - Clipped(count).Y
+                Dim x = left
+                Dim y = Clipped(count).Y + dy * (left - Clipped(count).X) / dx
+                Dim p As New Point(x, y)
+                If (Clipped(count).X < left And Clipped(count + 1).X > left) Then
+                    NewClipped.Add(p)
+                    NewClipped.Add(Clipped(count + 1))
+                Else
+                    NewClipped.Add(p)
+                End If
+            ElseIf (Clipped(count).X > left And Clipped(count + 1).X > left) Then
+                NewClipped.Add(Clipped(count + 1))
+            End If
+        Next
+
+        If ((Clipped(Clipped.ToArray.Length - 1).X > left And Clipped(0).X < left) Or (Clipped(Clipped.ToArray.Length - 1).X < left And Clipped(0).X > left)) Then
+            Dim dx = Clipped(0).X - Clipped(Clipped.ToArray.Length - 1).X
+            Dim dy = Clipped(0).Y - Clipped(Clipped.ToArray.Length - 1).Y
+            Dim y = Clipped(Clipped.ToArray.Length - 1).Y + dy * (left - Clipped(Clipped.ToArray.Length - 1).X) / dx
+            Dim x = left
+            Dim p As New Point(x, y)
+            If (Clipped(Clipped.ToArray.Length - 1).X < left And Clipped(0).X > left) Then
+                NewClipped.Add(p)
+                NewClipped.Add(Clipped(0))
+            Else
+                NewClipped.Add(p)
+            End If
+        ElseIf (Clipped(Clipped.ToArray.Length - 1).X > left And Clipped(0).X > left) Then
+            NewClipped.Add(Clipped(0))
+        End If
+
+        Clipped = NewClipped
+        NewClipped = New List(Of Point)
+        Clipping = Clipped
+    End Function
+
+    Private Sub pbCanvas_Paint(sender As Object, e As PaintEventArgs) Handles pbCanvas.Paint
+        'e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
+        If (Polygons IsNot Nothing) Then
+            e.Graphics.DrawPolygon(Pens.Blue, Polygons.ToArray())
+        End If
+        e.Graphics.DrawRectangle(_pen, SelectRect)
+        If (shape = "Polygon") Then
+            ' Draw the new polygon.
+            If (NewPolygon IsNot Nothing) Then
+                ' Draw the new polygon.
+                If (NewPolygon.Count > 1) Then
+                    e.Graphics.DrawLines(Pens.Green,
+                        NewPolygon.ToArray())
+                End If
+
+                ' Draw the newest edge.
+                If (NewPolygon.Count > 0) Then
+                    Using dashed_pen As New Pen(Color.Green)
+                        dashed_pen.DashPattern = New Single() {3, 3}
+                        e.Graphics.DrawLine(dashed_pen,
+                            NewPolygon(NewPolygon.Count - 1),
+                            NewPoint)
+                    End Using
+                End If
+            End If
+        End If
+        If (Polygons IsNot Nothing) Then
+            Dim Clipped As List(Of Point) = Clipping(Polygons.ToArray())
+            e.Graphics.DrawPolygon(Pens.Red, Clipped.ToArray())
+        End If
+    End Sub
+
+    Private Sub btnPoly_MouseClick(sender As Object, e As MouseEventArgs) Handles btnPoly.MouseClick
+        shape = "Polygon"
+    End Sub
+
+    Private Sub btnRect_MouseClick(sender As Object, e As MouseEventArgs) Handles btnRect.MouseClick
+        shape = "Rectangle"
+    End Sub
+End Class
